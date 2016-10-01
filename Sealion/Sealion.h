2f46e22db827b2ce0b498247359f4d6180da91c5
Dima Bart <dima.bart01@gmail.com>
2016-10-01 17:10:28 -0400

    Initial Commit

---
+++ Sealion/Sealion.h
@@ -0,0 +1,19 @@
+//
+//  Sealion.h
+//  Sealion
+//
+//  Created by Dima Bart on 2016-10-01.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sealion.
+FOUNDATION_EXPORT double SealionVersionNumber;
+
+//! Project version string for Sealion.
+FOUNDATION_EXPORT const unsigned char SealionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sealion/PublicHeader.h>
+
+
