26ea9be7182de84f027ec72e8bc573133d529746
Dima Bart <dima.bart01@gmail.com>
2016-10-08 00:09:09 -0400

    Modify JsonManager to allow deep aliases. Add Droplet tests.

---
+++ SealionTests/DropletTests.swift
@@ -0,0 +1,51 @@
+//
+//  DropletTests.swift
+//  Sealion
+//
+//  Created by Dima Bart on 2016-10-07.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+import XCTest
+@testable import Sealion
+
+class DropletTests: ModelTestCase {
+    
+    // ----------------------------------
+    //  MARK: - JsonCreatable -
+    //
+    func testJsonCreation() {
+        let model:     Droplet      = self.modelNamed(name: "droplet")
+        let kernel:    Kernel       = self.modelNamed(name: "kernel")
+        let window:    BackupWindow = self.modelNamed(name: "backupWindow")
+        let image:     Image        = self.modelNamed(name: "image")
+        let size:      Droplet.Size = self.modelNamed(name: "size")
+        let region:    Region       = self.modelNamed(name: "region")
+        let v4Network: Network      = self.modelNamed(name: "v4Network")
+        let v6Network: Network      = self.modelNamed(name: "v6Network")
+        
+        
+        XCTAssertEqual(model.id,          123)
+        XCTAssertEqual(model.name,        "some.droplet.com")
+        XCTAssertEqual(model.locked,      false)
+        XCTAssertEqual(model.status,      "active")
+        XCTAssertEqual(model.features,    ["virtio"])
+        XCTAssertEqual(model.tags,        ["awesome"])
+        XCTAssertEqual(model.backupIDs,   [123, 234])
+        XCTAssertEqual(model.snapshotIDs, [345, 456])
+        XCTAssertEqual(model.volumeIDs,   [567, 678])
+        XCTAssertEqual(model.createdAt,   Date(ISOString: "2015-08-07T19:50:26Z"))
+        
+        XCTAssertEqual(model.kernel,     kernel)
+        XCTAssertEqual(model.nextWindow, window)
+        XCTAssertEqual(model.image,      image)
+        XCTAssertEqual(model.size,       size)
+        XCTAssertEqual(model.region,     region)
+        XCTAssertEqual(model.v4Networks, [v4Network])
+        XCTAssertEqual(model.v6Networks, [v6Network])
+    }
+    
+    func testEquality() {
+        self.assertEqualityForModelNamed(type: Droplet.self, name: "droplet")
+    }
+}
--- SealionTests/JsonManager.swift
+++ SealionTests/JsonManager.swift
@@ -11,36 +11,63 @@ import Sealion
 
 class JsonManager {
     
-    private let json: JSON
+    private var originalJson: JSON!
+    private var expandedJson: JSON!
     
     // ----------------------------------
     //  MARK: - Init -
     //
     init(jsonNamed: String) {
-        let fileURL  = Bundle(for: JsonManager.self).url(forResource: jsonNamed, withExtension: "json")!
-        let fileData = try! Data(contentsOf: fileURL)
-        self.json    = try! JSONSerialization.jsonObject(with: fileData, options: []) as! JSON
+        let result        = JsonManager.preprocessJsonNamed(jsonNamed)
+        self.originalJson = result.original
+        self.expandedJson = result.processed
     }
     
     // ----------------------------------
     //  MARK: - Getters -
     //
     func modelJsonFor(key: String, expandAliases: Bool = true) -> JSON {
-        var json = self.json[key] as! JSON
-        
         if expandAliases {
-            for (key, value) in json {
-                
-                if let string = value as? String,
-                    string.hasPrefix("###") {
-                    
-                    let index = string.index(string.startIndex, offsetBy: 3)
-                    let alias = string.substring(from: index)
-                    
-                    json[key] = self.json[alias]
-                }
-            }
+            return self.expandedJson[key] as! JSON
+        } else {
+            return self.originalJson[key] as! JSON
         }
-        return json
+    }
+    
+    // ----------------------------------
+    //  MARK: - Preprocessing Json -
+    //
+    static func preprocessJsonNamed(_ jsonNamed: String) -> (original: JSON, processed: JSON) {
+        let fileURL    = Bundle(for: JsonManager.self).url(forResource: jsonNamed, withExtension: "json")!
+        let fileData   = try! Data(contentsOf: fileURL)
+        var fileString = String(data: fileData, encoding: .utf8)! as NSString
+        let original   = try! JSONSerialization.jsonObject(with: fileData, options: []) as! JSON
+        
+        let regex   = try! NSRegularExpression(pattern: "\"###(.+?)\"", options: [])
+        let results = regex.matches(in: fileString as String, options: [], range: NSRange(location: 0, length: fileString.length))
+        
+        for result in results.reversed() {
+            guard result.numberOfRanges > 1 else {
+                continue
+            }
+            
+            let range  = result.rangeAt(1)
+            let alias  = fileString.substring(with: range)
+            
+            guard let object = original[alias] else {
+                fatalError("Alias not found: \(alias)")
+            }
+            
+            let data   = try! JSONSerialization.data(withJSONObject: object, options: [])
+            let string = String(data: data, encoding: .utf8)!
+            
+            let replacementRange = result.rangeAt(0)
+            fileString = fileString.replacingCharacters(in: replacementRange, with: string) as NSString
+        }
+        
+        let processedData = (fileString as String).data(using: .utf8)
+        let processed     = try! JSONSerialization.jsonObject(with: processedData!, options: []) as! JSON
+        
+        return (original, processed)
     }
 }
