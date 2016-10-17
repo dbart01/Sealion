f7b8104685c8596251bb9e69f1ac306b7c16ec30
Dima Bart <dima.bart01@gmail.com>
2016-10-17 08:00:07 -0400

    Start on integration tests.

---
+++ SealionTests/IntegrationTests.swift
@@ -0,0 +1,82 @@
+//
+//  IntegrationTests.swift
+//  Sealion
+//
+//  Created by Dima Bart on 2016-10-16.
+//  Copyright © 2016 Dima Bart. All rights reserved.
+//
+
+import XCTest
+import Sealion
+
+class IntegrationTests: XCTestCase {
+    
+    typealias Block = () -> Void
+    
+    let api = API(version: .v2, token: "a4b2f1b7e1f10dab178375189f2a285a18abee5f4e353dcfedae7087e9e25463")
+    
+    // ----------------------------------
+    //  MARK: - Everything -
+    //
+    func testEverything() {
+        self.fetchImages()
+        let droplet = self.createDroplet()
+        self.deleteDroplet(droplet: droplet)
+    }
+    
+    // ----------------------------------
+    //  MARK: - Images -
+    //
+    private func fetchImages() {
+        let e = self.expectation(description: "Fetch Images")
+        
+        let handle = self.api.images(type: .distribution, page: Page(index: 0, count: 200)) { result in
+            print("Result: \(result)")
+            e.fulfill()
+        }
+        handle.resume()
+        
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+    
+    // ----------------------------------
+    //  MARK: - Droplet -
+    //
+    private func createDroplet() -> Droplet {
+        let e = self.expectation(description: "Create droplet")
+        
+        var createdDroplet: Droplet?
+        
+        let request = Droplet.CreateRequest(name: "test-droplet", region: "nyc3", size: "512mb", image: "ubuntu-14-04-x64")
+        let handle  = self.api.create(droplet: request) { result in
+            switch result {
+            case .success(let droplet):
+                createdDroplet = droplet!
+            case .failure(let error):
+                XCTFail(error?.description ?? "Failed to create droplet")
+            }
+            e.fulfill()
+        }
+        handle.resume()
+    
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+        return createdDroplet!
+    }
+    
+    private func deleteDroplet(droplet: Droplet) {
+        let e = self.expectation(description: "Delete droplet")
+        
+        let handle  = self.api.delete(droplet: droplet.id) { result in
+            switch result {
+            case .success:
+                break
+            case .failure(let error):
+                XCTFail(error?.description ?? "Failed to delete droplet")
+            }
+            e.fulfill()
+        }
+        handle.resume()
+        
+        self.waitForExpectations(timeout: 10.0, handler: nil)
+    }
+}
